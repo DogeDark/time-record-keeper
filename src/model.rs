e47d9e68801d78d4b4cac96257c48ac118a16146
Miles Murgaw <milesmurgaw@gmail.com>
2024-04-04 22:05:39 -0400

    commit: stashed changes

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,21 @@
 use chrono::NaiveDate;
+use rusqlite::Connection;
 
-#[derive(Debug, Clone, PartialEq)]
 pub struct SavedRecord {
+    pub record_id: u32,
+    pub client_id: u32,
     pub date: NaiveDate,
     pub time: u32,
     pub description: String,
+}
+
+pub struct Client {
+    pub client_id: u32,
+    pub client_name: String,
+}
+
+impl Client {
+    pub fn fetch_all(conn: &Connection) -> Vec<Client> {
+        let stmt = conn.execute("SELECT * FROM clients");
+    }
 }
